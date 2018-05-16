78b01354d0ef4a89f5b5419eab47d5989b14c153
erikwaahlstrom <erik93_93@hotmail.com>
2018-05-16 10:09:25 +0200

    dynamic listing of fake server playlist data

--- src/App.js
+++ src/App.js
@@ -83,14 +83,15 @@ class Filter extends Component {
 
 class Playlist extends Component {
 	render(){
+		let playlist = this.props.playlist
 		return(
 			<div style={{...defaultStyle, display: 'inline-block', width: "25%"}}>
 				<img />
-				<h3>Playlist Name</h3>
+				<h3>{playlist.name}</h3>
 				<ul>
-					<li>Song 1</li>
-					<li>Song 2</li>
-					<li>Song 3</li>
+					{playlist.songs.map(song =>
+						<li>{song.name}</li>
+						)}
 				</ul>
 			</div>
 			);
@@ -109,7 +110,7 @@ class App extends Component {
 		}, 1000);
 	}
 
-	render() {
+	render() {	
 		return (
 			<div className="App">
 
@@ -120,12 +121,10 @@ class App extends Component {
 						</h1>
 						<PlaylistCounter playlists={this.state.serverData.user.playlists} />
 						<HoursCounter playlists={this.state.serverData.user.playlists} />
-
-					<Filter />
-					<Playlist />
-					<Playlist />
-					<Playlist />
-					<Playlist />
+						<Filter />
+						{this.state.serverData.user.playlists.map(playlist => 
+							<Playlist playlist={playlist}/>
+						)}
 				</div> : <h1 style={defaultStyle}>Loading...</h1>
 				}
 			</div>
